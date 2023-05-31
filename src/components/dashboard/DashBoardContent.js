fae2505fc1bce0406c2420fb7c0e9af6c37fb691
Russel Harvey Mercado IV <russelharvey.mercado@cit.edu>
2023-05-31 08:40:55 +0800

    add extra navigation under manage

--- src/components/dashboard/DashBoardContent.js
+++ src/components/dashboard/DashBoardContent.js
@@ -3,15 +3,18 @@ import Card from "../UI/Card/Card";
 
 const DashBoardContent = () => {
   return (
-    <section className={classes.dashboardCards}>
-      <Card className={classes.item1}>
-        <h1>357</h1>
-        <h2>Rooms</h2>
-      </Card>
-      <Card className={classes.item2} />
-      <Card className={classes.item3} />
-      <Card className={classes.item4} />
-    </section>
+    <>
+      <section className={classes.dashboardCards}>
+        <Card className={classes.item1}>
+          <h1>357</h1>
+          <h2>Rooms</h2>
+        </Card>
+        <Card className={classes.item2} />
+        <Card className={classes.item3} />
+        <Card className={classes.item4} />
+      </section>
+      <div className={classes.buildingList}></div>
+    </>
   );
 };
 
--- src/components/dashboard/Manage.js
+++ src/components/dashboard/Manage.js
@@ -1,56 +1,88 @@
 import classes from "./Manage.module.css";
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialState = {
+  usersContent: true,
+  buildingContent: false,
+  roomContent: false,
+  spaceContent: false,
+  _5sContent: false,
+};
+
+const contentReducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_USERS":
+      return { ...initialState, usersContent: true };
+    case "TOGGLE_BUILDING":
+      return { ...initialState, buildingContent: true, usersContent: false };
+    case "TOGGLE_ROOM":
+      return { ...initialState, roomContent: true, usersContent: false };
+    case "TOGGLE_SPACE":
+      return { ...initialState, spaceContent: true, usersContent: false };
+    case "TOGGLE_5S":
+      return { ...initialState, _5sContent: true, usersContent: false };
+    default:
+      return state;
+  }
+};
 
 const Manage = () => {
-  const [usersContent, toggleUsersContent] = useState(true);
-  const [buildingContent, toggleBuildingContent] = useState(false);
-  const [roomContent, toggleRoomContent] = useState(false);
-
-  const toggleUsers = () => {
-    toggleUsersContent(true);
-    toggleBuildingContent(false);
-    toggleRoomContent(false);
-  };
-  const toggleBuilding = () => {
-    toggleBuildingContent(true);
-    toggleRoomContent(false);
-    toggleUsersContent(false);
-  };
-  const toggleRoom = () => {
-    toggleRoomContent(true);
-    toggleBuildingContent(false);
-    toggleUsersContent(false);
-  };
+  const [state, dispatch] = useReducer(contentReducer, initialState);
 
   return (
-    <nav>
-      <ul className={classes.manageNav}>
-        <li>
-          <button
-            onClick={toggleUsers}
-            className={`${usersContent ? classes.active : ""}`}
-          >
-            Users
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={toggleBuilding}
-            className={`${buildingContent ? classes.active : ""}`}
-          >
-            Building
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={toggleRoom}
-            className={`${roomContent ? classes.active : ""}`}
-          >
-            Room
-          </button>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <ul className={classes.manageNav}>
+          <li>
+            <button
+              onClick={() => dispatch({ type: "TOGGLE_USERS" })}
+              className={`${state.usersContent ? classes.active : ""}`}
+            >
+              Users
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => dispatch({ type: "TOGGLE_BUILDING" })}
+              className={`${state.buildingContent ? classes.active : ""}`}
+            >
+              Building
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => dispatch({ type: "TOGGLE_ROOM" })}
+              className={`${state.roomContent ? classes.active : ""}`}
+            >
+              Room
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => dispatch({ type: "TOGGLE_SPACE" })}
+              className={`${state.spaceContent ? classes.active : ""}`}
+            >
+              Space
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => dispatch({ type: "TOGGLE_5S" })}
+              className={`${state._5sContent ? classes.active : ""}`}
+            >
+              5s
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <main className={classes.manageContent}>
+        {state.usersContent ? "Users" : ""}
+        {state.buildingContent ? "Building" : ""}
+        {state.roomContent ? "Room" : ""}
+        {state.spaceContent ? "Space" : ""}
+        {state._5sContent ? "5s Archive" : ""}
+      </main>
+    </>
   );
 };
 export default Manage;

0851650cfd44f968557879db574ed5879868194c
Russel Harvey Mercado IV <russelharvey.mercado@cit.edu>
2023-06-01 12:55:52 +0800

    fix user view under manage

--- src/components/dashboard/userContent/UserContent.js
+++ src/components/dashboard/userContent/UserContent.js
@@ -1,17 +1,17 @@
-import Mock_data from "../../MOCK_DATA.json";
+import React, { useState, useCallback, useEffect } from "react";
 import ReactDom from "react-dom";
+import axios from "axios";
+
 import Table from "../../UI/table/Table";
+import GlobalFilter from "../../GlobalFilter";
+import Backdrop from "../../UI/Modal/BackdropModal";
+import Overlay from "../../UI/Modal/Overlay";
+
 import classes from "./UserContent.module.css";
 import action from "../../../static/images/link.png";
 import deleteIcon from "../../../static/images/delete.png";
 import editIcon from "../../../static/images/edit.png";
-
-import React, { useState, useCallback, useEffect } from "react";
-import GlobalFilter from "../../GlobalFilter";
-import Backdrop from "../../UI/Modal/BackdropModal";
-
-import axios from "axios";
-import Overlay from "../../UI/Modal/Overlay";
+import addIcon from "../../../static/images/add-user.png";
 
 const UserContent = () => {
   const [actionBtns, setActionBtns] = useState({});
@@ -19,12 +19,14 @@ const UserContent = () => {
   const [filter, setFilter] = useState({ filter: "default", setFil: () => {} });
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
+  const [isAdd, setIsAdd] = useState(false);
   const [isDelete, setIsDelete] = useState(false);
   const [refreshData, setRefreshData] = useState(false);
+  const [clickedData, setClickedData] = useState();
 
   const ActionBtnHandler = useCallback(
-    (rowId) => {
-      console.log(actionBtns);
+    (rowId, data) => {
+      setClickedData(data);
       setActionBtns((prevState) => ({
         ...Object.keys(prevState).reduce((acc, key) => {
           acc[key] = false;
@@ -33,7 +35,7 @@ const UserContent = () => {
         [rowId]: !prevState[rowId],
       }));
     },
-    [setActionBtns, actionBtns]
+    [setActionBtns]
   );
 
   const fetchUsers = useCallback(async () => {
@@ -43,10 +45,7 @@ const UserContent = () => {
 
   const deleteUser = async (id) => {
     try {
-      const response = await axios.delete(
-        `https://localhost:7124/api/user/${id}`
-      );
-      console.log(response);
+      await axios.delete(`https://localhost:7124/api/user/${id}`);
       setRefreshData(true);
     } catch (error) {
       console.log(error);
@@ -55,12 +54,17 @@ const UserContent = () => {
 
   const updateUser = async (id, data) => {
     try {
-      const response = await axios.put(
-        `https://localhost:7124/api/user/${id}`,
-        data
-      );
-      console.log(response);
-      setRefreshData(true);
+      await axios.put(`https://localhost:7124/api/user/${id}`, { ...data });
+      setRefreshData(!refreshData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addUser = async (data) => {
+    try {
+      await axios.post(`https://localhost:7124/api/user`, { ...data });
+      setRefreshData(!refreshData);
     } catch (error) {
       console.log(error);
     }
@@ -69,7 +73,6 @@ const UserContent = () => {
   useEffect(() => {
     fetchUsers()
       .then((list) => {
-        console.log(list);
         setUserData(list);
         setRefreshData(false);
       })
@@ -79,51 +82,33 @@ const UserContent = () => {
   }, [fetchUsers, refreshData]);
 
   const m_columns = [
-    {
-      Header: "Id",
-      accessor: "id",
-    },
-    {
-      Header: "Last Name",
-      accessor: "lastName",
-    },
-    {
-      Header: "First Name",
-      accessor: "firstName",
-    },
-    {
-      Header: "Username",
-      accessor: "username",
-    },
+    { Header: "Id", accessor: "id" },
+    { Header: "Last Name", accessor: "lastName" },
+    { Header: "First Name", accessor: "firstName" },
+    { Header: "Username", accessor: "username" },
     {
       Header: "Actions",
-      Cell: ({ row }) => {
-        return (
-          <div className={classes.actionCell}>
-            {!actionBtns[row.original["id"]] ? (
-              <button
-                onClick={() => {
-                  ActionBtnHandler(row.original["id"]);
-                }}
-                className={classes.actionBtn}
-              >
-                <img src={action} alt="actionIcon" />
+      Cell: ({ row }) => (
+        <div className={classes.actionCell}>
+          {!actionBtns[row.original["id"]] ? (
+            <button
+              onClick={() => ActionBtnHandler(row.original["id"], row.original)}
+              className={classes.actionBtn}
+            >
+              <img src={action} alt="actionIcon" />
+            </button>
+          ) : (
+            <div className={`${classes.actionBtnChoices} ${classes.actionBtn}`}>
+              <button onClick={onEditUser}>
+                <img src={editIcon} alt="editIcon" />
               </button>
-            ) : (
-              <div
-                className={classes.actionBtnChoices + " " + classes.actionBtn}
-              >
-                <button onClick={onEditUser}>
-                  <img src={editIcon} alt="editIcon" />
-                </button>
-                <button onClick={onDeleteUser}>
-                  <img src={deleteIcon} alt="deleteIcon" />
-                </button>
-              </div>
-            )}
-          </div>
-        );
-      },
+              <button onClick={onDeleteUser}>
+                <img src={deleteIcon} alt="deleteIcon" />
+              </button>
+            </div>
+          )}
+        </div>
+      ),
     },
   ];
 
@@ -131,6 +116,7 @@ const UserContent = () => {
     setIsModalOpen(true);
     setIsDelete(true);
   };
+
   const onEditUser = () => {
     setIsModalOpen(true);
     setIsEdit(true);
@@ -140,6 +126,13 @@ const UserContent = () => {
     setIsModalOpen(false);
     setIsDelete(false);
     setIsEdit(false);
+    setIsAdd(false);
+    setActionBtns({});
+  };
+
+  const onAddUser = () => {
+    setIsModalOpen(true);
+    setIsAdd(true);
   };
 
   const filterHandler = (fil, setFil) => {
@@ -157,18 +150,30 @@ const UserContent = () => {
           {ReactDom.createPortal(
             <Overlay
               onDelete={deleteUser}
+              onUpdate={updateUser}
               onConfirm={closeModal}
+              onCreate={addUser}
+              data={clickedData}
               userId={Object.keys(actionBtns).find(
                 (key) => actionBtns[key] === true
               )}
-              status={`${isDelete ? "delete" : "edit"}`}
+              status={
+                `${isDelete ? "delete" : ""}` ||
+                `${isEdit ? "edit" : ""}` ||
+                `${isAdd ? "create" : ""}`
+              }
             />,
             document.getElementById("overlay-root")
           )}
         </>
       )}
       <header className={classes.tableHeader}>
-        <h1>User</h1>
+        <div className={classes.createUser}>
+          <h1>User</h1>
+          <button onClick={onAddUser} className={classes.addBtn}>
+            <img src={addIcon} alt="addIcon" />
+          </button>
+        </div>
         <GlobalFilter filter={filter.filter} setFilter={filter.setFil} />
       </header>
       <Table

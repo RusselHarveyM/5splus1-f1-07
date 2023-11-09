70bd8bfcb8329cd387ef68a0c75ca6bb9a55c264
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-11-09 21:33:32 +0800

    make total space score variable

--- src/components/rooms/room/SpaceNavContent.js
+++ src/components/rooms/room/SpaceNavContent.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./SpaceNavContent.module.css";
 import Card from "../../UI/Card/Card";
 
 const SpaceNavContent = (props) => {
+  const [spaceTotalScore, setSpaceTotalScore] = useState(10);
+
   console.log("props {}{}{{}", props);
   return (
     <Card className={classes.spaceNavigation_content}>
@@ -22,42 +24,42 @@ const SpaceNavContent = (props) => {
             <h3>SORT</h3>
           </div>
           {/* score here */}
-          <h3>/10</h3>
+          <h3>/{spaceTotalScore}</h3>
         </Card>
         <Card className={classes.sio}>
           <div className={classes.scoreTitle}>
             <h3>SET IN ORDER</h3>
           </div>
           {/* score here */}
-          <h3>/10</h3>
+          <h3>/{spaceTotalScore}</h3>
         </Card>
         <Card className={classes.shine}>
           <div className={classes.scoreTitle}>
             <h3>SHINE</h3>
           </div>
           {/* score here */}
-          <h3>/10</h3>
+          <h3>/{spaceTotalScore}</h3>
         </Card>
         <Card className={classes.standardize}>
           <div className={classes.scoreTitle}>
             <h3>STANDARDIZE</h3>
           </div>
           {/* score here */}
-          <h3>/10</h3>
+          <h3>/{spaceTotalScore}</h3>
         </Card>
         <Card className={classes.sustain}>
           <div className={classes.scoreTitle}>
             <h3>SUSTAIN</h3>
           </div>
           {/* score here */}
-          <h3>/10</h3>
+          <h3>/{spaceTotalScore}</h3>
         </Card>
         <Card className={classes.safety}>
           <div className={classes.scoreTitle}>
             <h3>SAFETY</h3>
           </div>
           {/* score here */}
-          <h3>/10</h3>
+          <h3>/{spaceTotalScore}</h3>
         </Card>
       </div>
     </Card>

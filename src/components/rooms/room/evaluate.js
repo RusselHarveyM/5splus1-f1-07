638a036e36c26655693d5840ba452d94ca474d54
genepaulz <zafra.genepaulm2013@gmail.com>
2023-11-10 05:20:47 +0800

    added evaluate

---
+++ src/components/rooms/room/evaluate.js
@@ -0,0 +1,31 @@
+import OpenAI from "openai";
+// import dotenv from "dotenv";
+// require('dotenv').config()
+
+// dotenv.config();
+const openai = new OpenAI({apiKey: process.env.REACT_APP_OPENAI_API_KEY});
+async function evaluate(base64url) {
+  
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "user",
+        content: [
+          { type: "text", text: "for the first four S in the 5s methodology rate the image from 1-10, answer only in short sentences." },
+          {
+            type: "image_url",
+            image_url: {
+              "url": base64url
+            },
+          },
+        ],
+      },
+    ],
+    max_tokens: 300
+  });
+  console.log(JSON.stringify(response));
+  return JSON.stringify(response);
+}
+
+export default { evaluate }
